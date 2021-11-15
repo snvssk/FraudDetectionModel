1407e90892fbe937bf13939e9d0764141c434e37
NavyaMohan <nvmhn93@gmail.com>
2021-11-15 07:32:42 -0800

    Update RF_LOF-Stratified.py
    Changed the input source
    Code fix

--- RF_LOF-Stratified.py
+++ RF_LOF-Stratified.py
@@ -6,48 +6,30 @@ from sklearn.metrics import average_precision_score, f1_score,confusion_matrix,a
 from sklearn.neighbors import LocalOutlierFactor
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import StratifiedKFold
+from datetime import datetime
+import pickle 
 import logging
+import warnings
+warnings.filterwarnings("ignore")
+
 
 FORMAT = '%(asctime)s:%(name)s:%(levelname)s - %(message)s'
 
-logging.basicConfig(filename='RF_Model_log.log',
+logging.basicConfig(filename='Model_log.log',
                             filemode='a',
                             format=FORMAT,
                             datefmt='%Y-%b-%d %X%z',
                             level=logging.INFO)
 logging.info('Info message')
 
-# Loading dataset
-df = pd.read_csv('gs://245_project/PS_20174392719_1491204439457_log.csv')
-
-# Feature extraction
-data = df.copy() #full data
-#data = df.sample(frac=0.1) # Sampled data
-
-# Merchant flag for source and dist
-data['OrigC']=data['nameOrig'].apply(lambda x: 1 if str(x).find('C')==0 else 0)
-data['DestC']=data['nameDest'].apply(lambda x: 1 if str(x).find('C')==0 else 0)
-
-# flag for transfer and cashout from type feature
-data['TRANSFER']=data['type'].apply(lambda x: 1 if x=='TRANSFER' else 0)
-data['CASH_OUT']=data['type'].apply(lambda x: 1 if x=='CASH_OUT' else 0)
-
-# Calculating Amount error
-data['OrigAmntErr']=(abs(data.oldbalanceOrg-data.newbalanceOrig)-data.amount)
-
-# droping those feature which are id type category and those which used for feature extraction
-droplist=['isFlaggedFraud','type','nameDest','nameOrig']
-MLData=data.drop(labels=droplist,axis=1)
-
-#saving to CSV
-MLData.to_csv('TransformedCreditCardFraudData.csv')
 
 #data input
-inputfile = 'TransformedCreditCardFraudData.csv'
+inputfile = 'gs://245_project/data_01112021.csv'
 
 class Model:
     def __init__(self, datafile = inputfile, model_type = None):
         self.df = pd.read_csv(datafile)
+#         self.df = self.df.sample(frac=0.01) # Size of data frame is reduced
         self.model_type = model_type
         logging.info('Data loaded, filename : {0}, rows : {1}, columns : {2}'.format(datafile,self.df.shape[0],self.df.shape[1]))
         if self.model_type == 'lof':   
@@ -92,7 +74,7 @@ class Model:
             y_prob=self.user_defined_model.predict_proba(self.X_test)
             y_pred=self.user_defined_model.predict(self.X_test) 
             logging.info('AUPRC : {}'.format(average_precision_score(self.y_test, y_prob[:, 1])))
-            logging.info('F1_score : {}'.foramt(f1_score(self.y_test,y_pred)))
+            logging.info('F1_score : {}'.format(f1_score(self.y_test,y_pred)))
             logging.info('Confusion Matrix : {}'.format(confusion_matrix(self.y_test,y_pred)))
             logging.info('accuracy_score : {}'.format(accuracy_score(self.y_test,y_pred)))
             logging.info('classification_report : {}'.format(classification_report(self.y_test,y_pred)))
@@ -109,23 +91,32 @@ class Model:
             logging.info('Fold : {}'.format(i))
             logging.info('TRAIN : {0}, TEST : {1}'.format(train_index,test_index))
             #Pick Selected Training and Testing index(row numbers) and create Traing and Testing Folds
-            X_train, X_test = X.iloc[train_index], X.iloc[test_index]
-            y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+            self.X_train, self.X_test = X.iloc[train_index], X.iloc[test_index]
+            self.y_train, self.y_test = y.iloc[train_index], y.iloc[test_index]
             i+=1
-            Model.stkflod_RF(self,X_train,y_train,X_test,y_test)
+            Model.stkflod_RF(self)
             
-    def stkflod_RF(self,X_train,y_train,X_test,y_test):   
-        Model.split(self,0.2)
+    def stkflod_RF(self):   
         Model.fit(self)
         Model.model_result(self)
+        
+#     def packagingModel(self):
+#         filename = './ModelPackages/' + datetime.now().strftime("%Y-%m-%d %H:%M") +'_'+ str(self.model_type) + '_model.pkl' 
+#         with open(filename, 'wb') as model_file: 
+#             pickle.dump(self.user_defined_model, model_file)
+#         logging.info('Model Saved, file name : {}'.format(filename))
+        
+    def packagingModel(self):
+        filename = './ModelPackages/' + datetime.now().strftime("%Y-%m-%d %H:%M") +'_'+ str(self.model_type) + '_model.pkl' 
+        with open(filename, 'wb') as model_file: 
+            pickle.dump(self.user_defined_model, model_file)
+        logging.info('Model Saved, file name : {}'.format(filename))
 
 if __name__ == '__main__':
     model_instance1 = Model(model_type = 'rf')
     model_instance1.split(0.2)
     model_instance1.fit()   
+#     model_instance1.packagingModel()
     model_instance1.model_result()
+    model_instance1.kfoldValidation()
 
-    model_instance2 = Model(model_type = 'lof')
-    model_instance2.split(0.2)
-    model_instance2.fit()
-    model_instance2.model_result(model_type = 'lof')
